0423945a9dd6f3fefeeac575ccd51bb65ccfd804
judebn <2685885536@qq.com>
2026-02-18 13:15:42 +0800

    Adapt to dolby atmos
    Change-Id: Ic93b686ac82f2a7b0af1c849d39e668a5cde9abf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,7 +175,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += \
+      $(COMMON_PATH)/sepolicy/vendor\
+      $(COMMON_PATH)/dolby/sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # System properties
